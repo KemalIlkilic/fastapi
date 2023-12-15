955aed548960348807e9df488c76472dcf74f64f
KemalIlkilic <kemalonur.ilkilic@bahcesehir.edu.tr>
2023-12-15 19:54:31 +0300

    Add CRUD operations for posts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import Body, FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -20,6 +20,12 @@ def find_post(id):
         if post["id"] == id:
             return post
 
+def find_index_post(id):
+    for index, post in enumerate(my_posts):
+        if post['id'] == id:
+            return index
+    return None
+
 
 
 @app.get("/")
@@ -30,7 +36,7 @@ def root():
 def get_posts():
     return {"data" : my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post ):
     post_dict = post.model_dump()
     #post_dict = post.dict()
@@ -41,4 +47,26 @@ def create_posts(post: Post ):
 @app.get("/posts/{id}")
 def get_post(id : int):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+        """ response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message" : f"post with id: {id} was not found"} """
     return {"post" : post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+    index = find_index_post(id)
+    if index is None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id : int, post: Post):
+    index = find_index_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"post" : post_dict}
